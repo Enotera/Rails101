cdb4c3dc6df1253864a161dc91c807fcccf1b3ca
Hsieh Wan Ting <Yuduki@gmail.com>
2016-02-21 19:31:21 +0800

    建立作者機制

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_group
+  before_action :member_required, only: [:new, :create]
 
   def new
     @post = @group.posts.new
@@ -7,6 +8,7 @@ class PostsController < ApplicationController
 
   def create
     @post = @group.posts.build(post_params)
+    @post.author = current_user
 
     if @post.save
       redirect_to group_path(@group), notice: "新增文章成功！"
@@ -16,11 +18,11 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = @group.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
   end
 
   def update
-    @post = @group.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
 
     if @post.update(post_params)
       redirect_to group_path(@group), notice: "修改文章成功！"
@@ -30,13 +32,20 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = @group.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
 
     @post.destroy
     redirect_to group_path(@group), alert: "文章已刪除"
   end
 
   private
+
+  def member_required
+    if !current_user.is_member_of?(@group)
+      flash[:warning] = "你不是這個討論版成員，不能發文喔！"
+      redirect_to group_path(@group)
+    end
+  end
 
   def find_group
     @group = Group.find (params[:group_id])
