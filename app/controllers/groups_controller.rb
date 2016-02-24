b9d0e73f1a60d732c47f37f30014a0a6797ad1e6
Yuduki <Yuduki@gmail.com>
2016-02-24 12:16:34 +0800

    使用者可以加入退出group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,6 +13,7 @@ class GroupsController < ApplicationController
     @group = current_user.groups.create(group_params)
 
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
@@ -42,6 +43,32 @@ class GroupsController < ApplicationController
     @group = current_user.groups.find(params[:id])
     @group.destroy
     redirect_to groups_path, alert: "討論版已刪除"
+  end
+
+    def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入討論版成功！"
+    else
+      flash[:warning] = "你已經是本討論版成員了"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本討論版！"
+    else
+      flash[:warning] = "你不是本討論版成員，無法退出。"
+    end
+
+    redirect_to group_path(@group)
   end
 
   private
